b7230d1d5d67a1f9d6d85feb474fcb50670e6b02
rakhmanhanna <rakhmanhanna@gmail.com>
2021-07-29 16:09:16 +0300

    1.hw

---
+++ hw.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// 1.
+
+let value = prompt('Введите число', 0);
+
+if (value > 0) {
+    alert("Значение больше нуля");
+} else if (value < 0) {
+    alert("Значение меньше нуля");
+} else if (value == 0) {
+    alert("Значение равно нулю");
+}
+

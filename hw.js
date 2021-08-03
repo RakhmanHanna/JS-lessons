d5577d915f0b08e1c8113143574c9ac4954f490d
rakhmanhanna <rakhmanhanna@gmail.com>
2021-08-03 12:45:45 +0300

    hw 2, 3

--- hw.js
+++ hw.js
@@ -1,16 +1,22 @@
 'use strict';
 
-// 1.
+// 3.
+let age = prompt('Возраст');
+if (age >= 14 && age <= 90);
 
-let value = +prompt('Введите число', 0);
-
-if (value > 0) {
-    alert("Значение больше нуля");
-} else if (value < 0) {
-    alert("Значение меньше нуля");
-} else if (value == 0) {
-    alert("Значение равно нулю");
+//2.
+let userName = prompt("Логин?", '');
+if (userName == 'Админ') {
+  let pass = prompt('Пароль?', '');
+  if (pass == '123456') {
+    alert( 'Добро пожаловать!' );
+  } else if (pass == '' || pass == null) {
+    alert( 'Вход отменен' );
+  } else {
+    alert( 'Пароль неверен' );
+  }
+} else if (userName == '' || userName == null) {
+  alert( 'Вход отменен' );
 } else {
-    alert(value);
+  alert( "Пользователь не найден" );
 }
-

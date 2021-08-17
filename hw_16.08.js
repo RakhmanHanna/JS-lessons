6bc028b1ee92cced949264ee710fb7b218d3c3ba
rakhmanhanna <rakhmanhanna@gmail.com>
2021-08-17 13:22:20 +0300

    hw_task2

--- hw_16.08.js
+++ hw_16.08.js
@@ -27,13 +27,13 @@ const fn = (arr) =>
 
       if (gender === 'Female')
         acc.women.push({
-          fullName: firstName + lastName,
+          fullName: firstName + ' ' + lastName,
           gender,
           ...rest,
         });
       if (gender === 'Male')
         acc.men.push({
-          fullName: firstName + lastName,
+          fullName: firstName + ' ' + lastName,
           gender,
           ...rest,
         });
@@ -44,3 +44,31 @@ const fn = (arr) =>
       men: [],
     }
   );
+
+// 2
+// todo: Функция принимает массив объектов.
+// todo: Возвращает массив пользователей, отсортированных по полю username в алфавитном порядке и возраст которых начинается от 21 года.
+// todo: Количество пользователей может быть не ограничено.
+
+const initialData = [
+  { username: 'Boris', age: 25 },
+  { username: 'Alex', age: 21 },
+  { username: 'Oleg', age: 14 },
+  { username: 'Dmitriy', age: 35 },
+  { username: 'Lida', age: 72 },
+];
+
+let sortArray = (array) => {
+  let newArr = array.filter((item) => item.age >= 21);
+  function sortObj(a, b) {
+    if (a.username > b.username) {
+      return 1;
+    } else if (b.username > a.username) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+  return newArr.sort(sortObj);
+};
+console.log(sortArray(initialData));
